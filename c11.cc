f0519bef9b9c6e1831688ea527877a1887560eb6
angie <wenhe2020@gmail.com>
2020-06-20 17:41:54 -0700

    my first code

---
+++ c11.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl << endl;
+
+int a,b,c;
+cout<<"give me two integer";
+ cin >>a>>b;
+c=a+b;
+cout<<a<< " " <<b<< " "<<c<< endl;
+
+
+
+
+    return 0; 
+}
